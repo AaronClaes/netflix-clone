68c91a7e207f1aca6b3a9ec30ef14c5068f87ff8
Aaron Claes <aaronclaes22@gmail.com>
2021-10-14 22:11:17 +0200

    feat: add frontend netflix

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5R256_6fN0rSgRQrpcdwGyTN8vMRMo8Y",
+  authDomain: "netflix-clone-59890.firebaseapp.com",
+  projectId: "netflix-clone-59890",
+  storageBucket: "netflix-clone-59890.appspot.com",
+  messagingSenderId: "296635188373",
+  appId: "1:296635188373:web:7708d330a40a881678479c",
+};
