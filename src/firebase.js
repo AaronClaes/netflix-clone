7172504c43e71168f03b60c2e96367050c5b05d4
Aaron Claes <aaronclaes22@gmail.com>
2021-10-15 22:30:51 +0200

    feat: add functional login/logout + profile page

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA5R256_6fN0rSgRQrpcdwGyTN8vMRMo8Y",
   authDomain: "netflix-clone-59890.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "296635188373",
   appId: "1:296635188373:web:7708d330a40a881678479c",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
